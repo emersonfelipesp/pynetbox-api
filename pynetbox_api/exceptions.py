0a33678ac2dbcc56f5c273486cce834d8581bca3
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-02-06 01:18:47 -0300

    Add status_code field to FastAPIException

--- pynetbox_api/exceptions.py
+++ pynetbox_api/exceptions.py
@@ -4,7 +4,9 @@ class  FastAPIException(Exception):
         message: str,
         detail: str | None = None,
         python_exception: str | None = None,
+        status_code: int = 400
     ):
         self.message = message
         self.detail = detail
         self.python_exception = python_exception
+        self.status_code = status_code
