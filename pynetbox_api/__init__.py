09f48d0c65599b36d57075465c73f30dd2ae2736
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-02-07 14:59:58 -0300

    Correct variable name on env-example from NETBOX_HOST to NETBOX_URL

--- pynetbox_api/__init__.py
+++ pynetbox_api/__init__.py
@@ -1,10 +1,1 @@
-from dotenv import load_dotenv
-import os
-
 from .session import nb
-
-# Load environment variables from .env file
-load_dotenv()
-
-NETBOX_URL = os.getenv('NETBOX_URL')
-NETBOX_TOKEN = os.getenv('NETBOX_TOKEN')
