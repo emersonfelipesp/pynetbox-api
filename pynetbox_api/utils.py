255325af28f311db233262b542649df65697df29
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-02-12 13:19:46 +0000

    create utils.ValueLabelSchema to be used by several different schemas.

--- pynetbox_api/utils.py
+++ pynetbox_api/utils.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
 
+class ValueLabelSchema(BaseModel):
+    value: str | None = None
+    label: str | None = None
+
 class StatusSchema(BaseModel):
     value: str | None = None
     label: str | None = None
