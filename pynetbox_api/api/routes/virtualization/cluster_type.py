47c1fb89c51eb6d6df5b6ed817cf631af346164c
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-02-13 13:19:07 +0000

    Fix path parameter name on cluster_type routes

--- pynetbox_api/api/routes/virtualization/cluster_type.py
+++ pynetbox_api/api/routes/virtualization/cluster_type.py
@@ -15,7 +15,7 @@ cluster_type_router = APIRouter(tags=['Virtualization / Cluster Type'])
 async def get_device_types() -> ClusterTypeSchemaList:
     return ClusterType().all()
 
-@cluster_type_router.get('/{device_type_id}')
+@cluster_type_router.get('/{cluster_type_id}')
 async def get_device_type(device_type_id: int) -> ClusterTypeSchema:
     return ClusterType().get(id=device_type_id)
 
@@ -27,10 +27,10 @@ async def create_device_type(device_type: ClusterTypeSchemaIn) -> ClusterTypeSch
 async def create_device_type_placeholder(use_placeholder: Annotated[bool | None, Query()] = True) -> ClusterTypeSchema:
     return ClusterType(use_placeholder=use_placeholder).object
 
-@cluster_type_router.put('/{device_type_id}')
+@cluster_type_router.put('/{cluster_type_id}')
 async def update_device_type(device_type_id: int, device_type: ClusterTypeSchema) -> JSONResponse:
     return ClusterType().update(id=device_type_id, json=device_type.model_dump(exclude_unset=True))
 
-@cluster_type_router.delete('/{device_type_id}')
+@cluster_type_router.delete('/{cluster_type_id}')
 async def delete_device_type(device_type_id: int) -> JSONResponse:
     return ClusterType().delete(id=device_type_id)
