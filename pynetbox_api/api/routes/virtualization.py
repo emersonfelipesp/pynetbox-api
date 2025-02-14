ec65a224882825bc9863658e07026a11f6f6de30
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-02-14 01:12:14 +0000

    Implement virtualization.VirtualDisk (API routes, schemas, etc.)

--- pynetbox_api/api/routes/virtualization.py
+++ pynetbox_api/api/routes/virtualization.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 from pynetbox_api.api import create_endpoints
-from pynetbox_api.virtualization import ClusterType, ClusterGroup, Cluster, VirtualMachine
+from pynetbox_api.virtualization import (
+    ClusterType, ClusterGroup, Cluster,
+    VirtualMachine, VirtualDisk
+)
 
 virtualization_router = APIRouter()
-for router in [ClusterType, ClusterGroup, Cluster, VirtualMachine]:
+for router in [ClusterType, ClusterGroup, Cluster, VirtualMachine, VirtualDisk]:
     # Create the endpoints for each router, using only the class_instance.
     create_endpoints(router)
     virtualization_router.include_router(router.api_router, prefix=router.prefix)
--- pynetbox_api/virtualization/__init__.py
+++ pynetbox_api/virtualization/__init__.py
@@ -2,3 +2,4 @@ from pynetbox_api.virtualization.cluster_type import ClusterType
 from pynetbox_api.virtualization.cluster_group import ClusterGroup
 from pynetbox_api.virtualization.cluster import Cluster
 from pynetbox_api.virtualization.virtual_machine import VirtualMachine
+from pynetbox_api.virtualization.virtual_disk import VirtualDisk
