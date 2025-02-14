00d3e5efdd033732efcff06cf49ddda6b49fbe7b
Emerson Felipe <emersonfelipe.2003@gmail.com>
2025-02-14 01:42:25 +0000

    Closes #6 - Implement virtualization.VMInterface (API routes, schemas, etc.)

--- pynetbox_api/api/routes/virtualization.py
+++ pynetbox_api/api/routes/virtualization.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter
 from pynetbox_api.api import create_endpoints
 from pynetbox_api.virtualization import (
     ClusterType, ClusterGroup, Cluster,
-    VirtualMachine, VirtualDisk
+    VirtualMachine, VirtualDisk, VMInterface
 )
 
 virtualization_router = APIRouter()
-for router in [ClusterType, ClusterGroup, Cluster, VirtualMachine, VirtualDisk]:
+for router in [ClusterType, ClusterGroup, Cluster, VirtualMachine, VirtualDisk, VMInterface]:
     # Create the endpoints for each router, using only the class_instance.
     create_endpoints(router)
     virtualization_router.include_router(router.api_router, prefix=router.prefix)
